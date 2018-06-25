845d00a75d96ed23aec23c7d41e6407806d4d8b5
Jeremy Parker <jrobinparker@gmail.com>
2018-06-25 21:31:40 +0000

    automatically backed up by learn

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,6 +7,6 @@ class Reservation < ActiveRecord::Base
   validates :checkout, presence: true  
 
   def total_price 
-    self.group(:checkin, :checkout)
+    days = self.group(:checkin, :checkout)
   
 end
