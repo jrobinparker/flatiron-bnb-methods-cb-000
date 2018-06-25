4de792da2f809bd4ac0e183e8565b79e25eeea67
Jeremy Parker <jrobinparker@gmail.com>
2018-06-25 21:16:14 +0000

    automatically backed up by learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,6 +2,8 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-
+   def most_res
+    @city.listing.order("value DESC").first
+  
 end
 
